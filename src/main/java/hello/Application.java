f7407e33d8b4dbf6950c6bec1c75f5ad213acda3
Shawn Lim <shawnlim89@live.com>
2019-05-04 15:25:57 +0800

    Add custom annotation

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -7,12 +7,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import java.lang.reflect.Method;
 
 @SpringBootApplication
 public class Application {
 
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
+    getTodoReport();
   }
 
   @Bean
@@ -30,4 +32,29 @@ public class Application {
     };
   }
 
+  private static void getTodoReport() {
+
+    Class<HelloController> helloControllerClass = HelloController.class;
+
+    Todo todoAnnotationClass = (Todo) helloControllerClass.getAnnotation(Todo.class);
+
+    if (todoAnnotationClass != null) {
+      System.out.println(" Class Name : " + helloControllerClass.getName());
+      System.out.println(" Author : " + todoAnnotationClass.author());
+      System.out.println(" Priority : " + todoAnnotationClass.priority());
+      System.out.println(" Status : " + todoAnnotationClass.status());
+      System.out.println(" --------------------------- ");
+    }
+
+    for (Method method : helloControllerClass.getMethods()) {
+      Todo todoAnnotation = (Todo) method.getAnnotation(Todo.class);
+      if (todoAnnotation != null) {
+        System.out.println(" Method Name : " + method.getName());
+        System.out.println(" Author : " + todoAnnotation.author());
+        System.out.println(" Priority : " + todoAnnotation.priority());
+        System.out.println(" Status : " + todoAnnotation.status());
+        System.out.println(" --------------------------- ");
+      }
+    }
+  }
 }
--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -4,9 +4,15 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
+@Todo(priority = Todo.Priority.LOW, author = "Caleb")
 public class HelloController {
   @RequestMapping("/")
   public String index() {
     return "Greetings from Spring Boot!";
+  }
+
+  @Todo(priority = Todo.Priority.HIGH)
+  public void notYetStartedMethod() {
+    // No Code Written yet
   }
 }
