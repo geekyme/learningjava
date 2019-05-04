8280e6289c32150976f9158cdd93860dd0a934cb
Shawn Lim <shawnlim89@live.com>
2019-05-04 15:57:21 +0800

    Add basic ability to query mongo

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -1,14 +1,27 @@
 package hello;
 
+import hello.models.Pets;
+import hello.repositories.PetsRepository;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @RestController
 @Todo(priority = Todo.Priority.LOW, author = "Caleb")
 public class HelloController {
+  @Autowired
+  private PetsRepository repository;
+
   @RequestMapping("/")
   public String index() {
     return "Greetings from Spring Boot!";
+  }
+
+  @RequestMapping("/pets")
+  public List<Pets> getAllPets() {
+    return repository.findAll();
   }
 
   @Todo(priority = Todo.Priority.HIGH)
